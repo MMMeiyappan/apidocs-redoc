8b50013719d43d72d51283b5abf70733fc439be6
MMMeiyappan <101340020+MMMeiyappan@users.noreply.github.com>
2022-12-18 09:51:30 +0530

    reacted all

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ root.render(
     <App />
   </React.StrictMode>
 );
-
+<script src="https://cdn.redoc.ly/redoc/latest/bundles/redoc.standalone.js"></script>
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
